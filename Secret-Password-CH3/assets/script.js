1c33702293568d96afa36dfad623c301e3b0a0a0
Neta2393 <benetheahardin@yahoo.com>
2023-04-20 20:20:30 -0700

    updated repo

--- Secret-Password-CH3/assets/script.js
+++ Secret-Password-CH3/assets/script.js
@@ -10,20 +10,27 @@ function writePassword() {
 
 }
 function generatePassword() {
+  // added array for numbers,letters and special characters
   var uppercasearray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
   var lowercasearray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
   var numberarray = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ]
   var specialarray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
-  let passwordLegnth = prompt("Please enter a password that is between 8 and 128 characters");
-  console.log (passwordLegnth)
-  if (passwordLegnth <8|| passwordLegnth >128){
-  alert("Wrong Legnth")
+  let passwordLength = prompt("Please enter a password that is between 8 and 128 characters")
+  console.log (passwordLength);
+  if (passwordLength <8|| passwordLength >128){
+  alert("Wrong Length")
   }
- let adduppercase = confirm("Would you like to add an uppercase letter?");
- let addlowercase = confirm("Would you like to add a lowercase letter?")
- let addnumber = confirm("Would you like to add a number?")
- let addspecialcharacter = confirm("Would you like to add a special character?")
-console.log (adduppercase)
 }
+  {
+ let adduppercase = confirm("Would you like to add an uppercase letter?")
+ console.log (adduppercase);
+ let addlowercase = confirm("Would you like to add a lowercase letter?")
+ console.log(addlowercase);
+ let addnumber = confirm("Would you like to add a number?")
+ console.log(addnumber);
+ let addspecialcharacter = confirm("Would you like to add a special character?")
+ console.log(addspecialcharacter);
+  }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
